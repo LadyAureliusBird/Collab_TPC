b4770aaa024b6832163b925e846e3f11f3d49aea
LadyAureliusBird <ethologica@proton.me>
2025-07-05 17:24:43 -0700

    Update TPC SideQuest 1.R

--- TPC SideQuest 1.R
+++ TPC SideQuest 1.R
@@ -20,16 +20,21 @@ atlas <- read_dta("atlas.dta")
 # Install and load package
 
 #Report weighted summary statistics
+#there is a problem with mean count, I copied this from the PDF 
+#this is the current error "'x' and 'w' must have the same length" 
 meanCount = weighted.mean(atlas$yvar, atlas$count_pooled)
 meanSD = sqrt(wtd.var(atlas$yvar,atlas$count_pooled))
 
 #set state and county FIPS code https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt 
+#manually confirm validation of state/county/tract 
 query_state <- 55
 query_county <- 079 
+query_tract <- 20100
 current_state <- subset(atlas,state == query_state)
 current_county <- subset(atlas,state == query_state & county == query_county)
+current_tract <- subset(atlas,state == query_state & county == query_county & tract == query_tract)
 
-
+p_pooled_var <- current_tract$kfr_pooled_p25
 
 
 
