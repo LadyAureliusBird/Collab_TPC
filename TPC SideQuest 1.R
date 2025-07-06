31ff0fdb45ac9f1002dd8e269e87bc297d3d3978
LadyAureliusBird <ethologica@proton.me>
2025-07-05 18:47:09 -0700

    Update TPC SideQuest 1.R

--- TPC SideQuest 1.R
+++ TPC SideQuest 1.R
@@ -32,7 +32,8 @@ query_county <- 079
 query_tract <- 20100
 current_state <- subset(atlas,state == query_state)
 current_county <- subset(atlas,state == query_state & county == query_county)
-current_tract <- subset(atlas,state == query_state & county == query_county & tract == query_tract)
+#removed state and county validation, tract is 'choose your own adventure' 
+current_tract <- subset(atlas,tract == query_tract)
 
 p_pooled_var <- current_tract$kfr_pooled_p25
 
